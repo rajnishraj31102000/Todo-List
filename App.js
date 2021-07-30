5415dd3a32a76e42673009db3d460933d2867227
Rajnish <48088684+rajnishraj31102000@users.noreply.github.com>
2021-07-30 21:40:27 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+//import logo from './logo.svg';
+import './App.css';
+import Todo from  './Mycomponents/Todo' ;
+import Footer from  './Mycomponents/Footer' ;
+
+function App() {
+  return (
+    <>
+   <div className="App" >
+       <Todo/>
+
+      
+
+       <Footer/>
+       
+       <h1>{56+198}</h1>
+
+    </div>
+    </>
+  );
+}
+
+export default App;
